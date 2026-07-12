e8193561de728d5113abcd088c5523ec3c08756a
karamtechnologies <karamtechnologies9@gmail.com>
2026-07-12 22:22:04 +0530

    port gmail changed

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,9 @@ app.post("/send-email", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
 
-      port: 465,
+      port: 587,
 
-      secure: true,
+      secure: false,
 
       auth: {
         user: process.env.EMAIL_USER,
